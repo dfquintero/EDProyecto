9b74ab60c82a7ee0c2502f334217e87d404fed08
crubioa <55598627+car1803@users.noreply.github.com>
2020-12-08 19:17:28 -0500

    Create validar.php

---
+++ FrontEnd/VUNMe/validar.php
@@ -0,0 +1,28 @@
+<?php
+    session_start();
+    $usuario=$_POST['usuario'];
+    $clave=$_POST['clave'];
+    $_SESSION['usuario'] = $usuario;
+    //conectar a la base
+    $conexion=mysqli_connect("localhost","root","","RecidenciaB");
+    $consulta="SELECT * FROM perfil WHERE nombre='$usuario' and contrasena='$clave'";
+    $resultado=mysqli_query($conexion, $consulta);
+
+    $filas=mysqli_num_rows($resultado);
+    if ($filas>0){
+        //inicia sesion
+        $identificador="SELECT ID_USUARIO FROM perfil WHERE email='$usuario'";
+        $valor="SELECT ID_USUARIO FROM estudiante WHERE ID_USUARIO='$identificador'";
+        if ($valor=$identificador){
+            header("location:estudiante.html")
+        }
+        else{
+            header("location:propietario.html")
+        }
+    }
+    else{
+        //error de autenticacion
+    }
+    mysqli_free_result($resultado);
+    mysqli_close($conexion);
+?>
